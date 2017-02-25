e25d6337abd1623d030f17ca415dc654e6943803
tcrane <thomasgcrane@gmail.com>
2017-02-25 21:33:47 +1100

    Added OnNewTick method:  - Added some more config fields  - Added the PostMessage command from the WinApi  - Added KeyCodes and Key up/down codes  - Changed targets field from a Queue to a List Added a Start method

--- FameBot/Core/Plugin.cs
+++ FameBot/Core/Plugin.cs
@@ -15,6 +15,8 @@ using Lib_K_Relay.Networking.Packets;
 using Lib_K_Relay.Networking.Packets.Server;
 using Lib_K_Relay.Networking.Packets.DataObjects;
 using Lib_K_Relay.Utilities;
+using Lib_K_Relay.Networking.Packets.Client;
+using FameBot.Services;
 
 namespace FameBot.Core
 {
@@ -46,22 +48,42 @@ namespace FameBot.Core
         #endregion
 
         private IntPtr flashPtr;
-        private bool followTarget = false;
-        private Queue<Target> targets;
+        private bool followTarget;
+        private List<Target> targets;
         private Dictionary<int, Target> playerPosisions;
         private Client connectedClient;
+        private int tickCount;
 
         private float autoNexusThreshold = 0.45f;
+        private int tickCountThreshold = 10;
+        private bool escapeIfNoTargets = true;
+        private float teleportDistanceThreshold = 15f;
+        private float followDistanceThreshold = 1.5f;
 
         #region WINAPI
         // Get the focused window
         [DllImport("user32.dll", SetLastError = true)]
         private static extern IntPtr GetForegroundWindow();
+        // Send a message to a specific process via the handle
+        [DllImport("user32.dll")]
+        public static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+        #endregion
+
+        #region KeyCodes
+        private bool W_PRESSED, A_PRESSED, S_PRESSED, D_PRESSED;
+
+        public const int WM_KEYDOWN = 0x0100;
+        public const int WM_KEYUP = 0x0101;
+
+        public const short VK_A = 0x41;
+        public const short VK_D = 0x44;
+        public const short VK_W = 0x57;
+        public const short VK_S = 0x53;
         #endregion
 
         public void Initialize(Proxy proxy)
         {
-            targets = new Queue<Target>();
+            targets = new List<Target>();
             playerPosisions = new Dictionary<int, Target>();
 
             Process[] processes = Process.GetProcessesByName("flash");
@@ -98,6 +120,10 @@ namespace FameBot.Core
                     flashPtr = GetForegroundWindow();
                     client.Notify("FameBot is now active");
                     break;
+                case "start":
+                    Start();
+                    client.Notify("FameBot is starting");
+                    break;
             }
         }
 
@@ -118,6 +144,18 @@ namespace FameBot.Core
         {
             followTarget = false;
             targets.Clear();
+        }
+
+        private void Start()
+        {
+            targets.Clear();
+            followTarget = true;
+        }
+
+        private void Escape(Client client)
+        {
+            Console.WriteLine("[FameBot] Escaping to nexus.");
+            client.SendToServer(Packet.Create(PacketType.ESCAPE));
         }
 
         #region PacketHookMethods
@@ -147,22 +185,177 @@ namespace FameBot.Core
             }
 
             // Remove old info
-            foreach(int dropId in packet.Drops)
+            foreach (int dropId in packet.Drops)
             {
                 if (playerPosisions.ContainsKey(dropId))
+                {
+                    if(followTarget && targets.Exists(t => t.ObjectId == dropId))
+                    {
+                        targets.Remove(targets.Find(t => t.ObjectId == dropId));
+                        if(targets.Count > 0)
+                        {
+                            Console.WriteLine("[FameBot] The player \"{0}\" was dropped from the target list.", playerPosisions[dropId].Name);
+                        } else
+                        {
+                            Console.WriteLine("[FameBot] There are no players left in the target list.");
+                            if (escapeIfNoTargets)
+                                Escape(client);
+                        }
+                    }
                     playerPosisions.Remove(dropId);
+                }
             }
         }
 
         private void OnNewTick(Client client, Packet p)
         {
             NewTickPacket packet = p as NewTickPacket;
+            tickCount++;
 
             // Autonexus
             float healthPercentage = (float)client.PlayerData.Health / (float)client.PlayerData.MaxHealth;
             if (healthPercentage < autoNexusThreshold)
-                client.SendToServer(Packet.Create(PacketType.ESCAPE));
+                Escape(client);
 
+            if(tickCount % tickCountThreshold == 0)
+            {
+                if (playerPosisions.Count > 0)
+                {
+                    targets = D36n4.Invoke(playerPosisions.Values.ToList());
+                    Console.WriteLine("[FameBot] Now targeting {0} players", targets.Count);
+                }
+                tickCount = 0;
+            }
+            
+            if(!followTarget)
+            {
+                if (W_PRESSED)
+                {
+                    W_PRESSED = false;
+                    PostMessage(flashPtr, WM_KEYUP, VK_W, 0);
+                }
+                if (A_PRESSED)
+                {
+                    A_PRESSED = false;
+                    PostMessage(flashPtr, WM_KEYUP, VK_A, 0);
+                }
+                if (S_PRESSED)
+                {
+                    S_PRESSED = false;
+                    PostMessage(flashPtr, WM_KEYUP, VK_S, 0);
+                }
+                if (D_PRESSED)
+                {
+                    D_PRESSED = false;
+                    PostMessage(flashPtr, WM_KEYUP, VK_D, 0);
+                }
+            }
+
+            if(followTarget && targets.Count > 0)
+            {
+                var targetPosition = new Location(targets.Average(t => t.Position.X), targets.Average(t => t.Position.Y));
+
+                if(client.PlayerData.Pos.DistanceTo(targetPosition) > teleportDistanceThreshold)
+                {
+                    TeleportPacket tpPacket = Packet.Create(PacketType.TELEPORT) as TeleportPacket;
+                    tpPacket.ObjectId = targets.OrderBy(t => t.Position.DistanceTo(targetPosition)).First().ObjectId;
+                    client.SendToServer(tpPacket);
+                }
+
+                #region Movement
+                // Left or right
+                if (client.PlayerData.Pos.X < targetPosition.X - followDistanceThreshold)
+                {
+                    // Move right
+                    if (!D_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYDOWN, VK_D, 0);
+                        D_PRESSED = true;
+                    }
+                    if (A_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYUP, VK_A, 0);
+                        A_PRESSED = false;
+                    }
+                }
+                else if (client.PlayerData.Pos.X <= targetPosition.X + followDistanceThreshold)
+                {
+                    if (D_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYUP, VK_D, 0);
+                        D_PRESSED = false;
+                    }
+                }
+                if (client.PlayerData.Pos.X > targetPosition.X + followDistanceThreshold)
+                {
+                    // Move left
+                    if (!A_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYDOWN, VK_A, 0);
+                        A_PRESSED = true;
+                    }
+                    if (D_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYUP, VK_D, 0);
+                        D_PRESSED = false;
+                    }
+                }
+                else if (client.PlayerData.Pos.X >= targetPosition.X - followDistanceThreshold)
+                {
+                    if (A_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYUP, VK_A, 0);
+                        A_PRESSED = false;
+                    }
+                }
+
+                // Up or down
+                if (client.PlayerData.Pos.Y < targetPosition.Y - followDistanceThreshold)
+                {
+                    // Move down
+                    if (!S_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYDOWN, VK_S, 0);
+                        S_PRESSED = true;
+                    }
+                    if (W_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYUP, VK_W, 0);
+                        W_PRESSED = false;
+                    }
+                }
+                else if (client.PlayerData.Pos.Y <= targetPosition.Y + followDistanceThreshold)
+                {
+                    if (S_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYUP, VK_S, 0);
+                        S_PRESSED = false;
+                    }
+                }
+                if (client.PlayerData.Pos.Y > targetPosition.Y + followDistanceThreshold)
+                {
+                    // Move up
+                    if (!W_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYDOWN, VK_W, 0);
+                        W_PRESSED = true;
+                    }
+                    if (S_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYUP, VK_S, 0);
+                        S_PRESSED = false;
+                    }
+                }
+                else if (client.PlayerData.Pos.Y >= targetPosition.Y - followDistanceThreshold)
+                {
+                    if (W_PRESSED)
+                    {
+                        PostMessage(flashPtr, WM_KEYUP, VK_W, 0);
+                        W_PRESSED = false;
+                    }
+                }
+                #endregion
+            }
         }
         #endregion
     }
