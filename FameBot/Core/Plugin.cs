5a35426a3d9e4d479b94e28ea81ffa81dff2fa90
tcrane <thomasgcrane@gmail.com>
2017-02-25 18:53:00 +1100

    Added Plugin.cs This is the file which is run by KRelay

---
+++ FameBot/Core/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using Lib_K_Relay.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib_K_Relay;
+using System.Diagnostics;
+
+namespace FameBot.Core
+{
+    public class Plugin : IPlugin
+    {
+        #region IPlugin
+        public string GetAuthor()
+        {
+            return "Chicken";
+        }
+
+        public string[] GetCommands()
+        {
+            return new string[]
+            {
+                "TODO: list commands"
+            };
+        }
+
+        public string GetDescription()
+        {
+            return "TODO: add description";
+        }
+
+        public string GetName()
+        {
+            return "FameBot by Chicken";
+        }
+        #endregion
+
+        public void Initialize(Proxy proxy)
+        {
+            
+        }
+    }
+}
