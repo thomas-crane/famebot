36a49e792ed5557b0891df2b5340b5718d01a1be
tcrane <thomasgcrane@gmail.com>
2017-02-25 18:35:53 +1100

    Added fields ctor and methods to Target

--- FameBot/Data/Models/Target.cs
+++ FameBot/Data/Models/Target.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lib_K_Relay.Networking.Packets.DataObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace FameBot.Data.Models
 {
-    class Target
+    public class Target
     {
+        private int objectId;
+        private string name;
+        private Location position;
+
+        public int ObjectId
+        {
+            get { return objectId; }
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public Location Position
+        {
+            get { return position; }
+        }
+
+        public Target(int objectId, string name, Location position)
+        {
+            this.objectId = objectId;
+            this.name = name;
+            this.position = position;
+        }
+
+        public void UpdatePosition(Location position)
+        {
+            this.position = position;
+        }
+
+        public float DistanceTo(Target target)
+        {
+            return Position.DistanceTo(target.Position);
+        }
     }
 }
