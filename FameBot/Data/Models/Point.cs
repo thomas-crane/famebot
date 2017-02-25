87fa8228ac897b1ae8c424b35417146c0930897f
tcrane <thomasgcrane@gmail.com>
2017-02-25 18:38:18 +1100

    Added properties ctor and methods to Point

--- FameBot/Data/Models/Point.cs
+++ FameBot/Data/Models/Point.cs
@@ -8,5 +8,26 @@ namespace FameBot.Data.Models
 {
     class Point
     {
+        public int ClusterId { get; set; }
+        public bool Noisy { get; set; }
+        public bool Visited { get; set; }
+        public Target Data { get; set; }
+
+        public Point(Target t)
+        {
+            this.Noisy = false;
+            this.Visited = false;
+            this.Data = t;
+        }
+
+        public static List<Point> FromListOfTargets(List<Target> targets)
+        {
+            List<Point> pointList = new List<Point>();
+            foreach(Target t in targets)
+            {
+                pointList.Add(new Point(t));
+            }
+            return pointList;
+        }
     }
 }
