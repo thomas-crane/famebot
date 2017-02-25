649ac7cc7b80908d793759fc7d5132d629d23859
tcrane <thomasgcrane@gmail.com>
2017-02-25 20:15:10 +1100

    Added ClientExtensions class  - Added Notify extension to send a notification to the client

---
+++ FameBot/Helpers/ClientExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Lib_K_Relay.Networking;
+using Lib_K_Relay.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FameBot.Helpers
+{
+    public static class ClientExtensions
+    {
+        public static void Notify(this Client client, string text)
+        {
+            client.SendToClient(PluginUtils.CreateNotification(client.ObjectId, text));
+        }
+    }
+}
