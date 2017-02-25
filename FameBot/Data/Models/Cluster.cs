71c091d42b967a18d3bab56542525c70394b8381
tcrane <thomasgcrane@gmail.com>
2017-02-25 18:42:39 +1100

    Added properties ctor and methods to Cluster

--- FameBot/Data/Models/Cluster.cs
+++ FameBot/Data/Models/Cluster.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace FameBot.Data.Models
 {
-    class Cluster
+    public class Cluster
     {
+        public int Id { get; set; }
+        public List<Point> Points { get; set; }
+        public float Epsilon { get; set; }
+        public int Count
+        {
+            get { return Points?.Count ?? 0; }
+        }
+
+        public Cluster()
+        {
+            Points = new List<Point>();
+            Epsilon = 8f;
+        }
+
+        public void Add(Point p)
+        {
+            Points.Add(p);
+        }
+
+        public void AddRange(List<Point> p)
+        {
+            Points.AddRange(p);
+        }
+
+        public void AddRange(Cluster c)
+        {
+            Points.AddRange(c.Points);
+        }
+
+        public bool Contains(Point p)
+        {
+            return Points.Contains(p);
+        }
     }
 }
