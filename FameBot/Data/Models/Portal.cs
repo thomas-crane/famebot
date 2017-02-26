31cf387e6fc669857b8291148141ac806c3e7e2c
tcrane <thomasgcrane@gmail.com>
2017-02-26 20:10:32 +1100

    Added Portal model

---
+++ FameBot/Data/Models/Portal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FameBot.Data.Models
+{
+    public class Portal
+    {
+        public int ObjectId { get; set; }
+        public int PlayerCount { get; set; }
+        public string Name { get; set; }
+        public bool Connected { get; set; }
+
+        public Portal(int objectId, int playerCount, string name)
+        {
+            ObjectId = objectId;
+            PlayerCount = playerCount;
+            Name = name;
+        }
+    }
+}
