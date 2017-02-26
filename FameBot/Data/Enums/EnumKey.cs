81c980aaaccec9aee732b0c33df8c0a965603869
tcrane <thomasgcrane@gmail.com>
2017-02-26 11:52:36 +1100

    Added EnumKey enum to hold keycodes

---
+++ FameBot/Data/Enums/EnumKey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FameBot.Data.Enums
+{
+    public enum Key
+    {
+        W = 0x57,
+        A = 0x41,
+        S = 0x53,
+        D = 0x44,
+        KeyUp = 0x0101,
+        KeyDown = 0x0100
+    }
+}
