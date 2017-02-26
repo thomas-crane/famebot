844d4842d9b22a1d0527c20c577c13161b446160
tcrane <thomasgcrane@gmail.com>
2017-02-26 12:13:56 +1100

    Added switch statement to change key images

--- FameBot/UserInterface/KeyPressGUI.cs
+++ FameBot/UserInterface/KeyPressGUI.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FameBot.Core;
+using FameBot.Data.Enums;
+using FameBot.Properties;
 
 namespace FameBot.UserInterface
 {
@@ -15,6 +18,36 @@ namespace FameBot.UserInterface
         public KeyPressGUI()
         {
             InitializeComponent();
+            Plugin.keyChanged += (s, e) =>
+            {
+                switch (e.Key)
+                {
+                    case Key.W:
+                        if (e.Value)
+                            wBox.Image = Resources.w_on;
+                        else
+                            wBox.Image = Resources.w_off;
+                        break;
+                    case Key.A:
+                        if (e.Value)
+                            aBox.Image = Resources.a_on;
+                        else
+                            aBox.Image = Resources.a_off;
+                        break;
+                    case Key.S:
+                        if (e.Value)
+                            sBox.Image = Resources.s_on;
+                        else
+                            sBox.Image = Resources.s_off;
+                        break;
+                    case Key.D:
+                        if (e.Value)
+                            dBox.Image = Resources.d_on;
+                        else
+                            dBox.Image = Resources.d_off;
+                        break;
+                }
+            };
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
