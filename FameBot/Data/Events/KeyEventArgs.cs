40f79a58205f637d378a26dbd7fcf0a20ac09c27
tcrane <thomasgcrane@gmail.com>
2017-02-26 11:54:41 +1100

    Added KeyEventArgs

---
+++ FameBot/Data/Events/KeyEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FameBot.Data.Enums;
+
+namespace FameBot.Data.Events
+{
+    public class KeyEventArgs : EventArgs
+    {
+        private Key key;
+        public Key Key
+        {
+            get { return key; }
+        }
+
+        public KeyEventArgs(Key key) : base()
+        {
+            this.key = key;
+        }
+    }
+}
