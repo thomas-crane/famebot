2bc257dc78cc6ba4a4bae5edde5dc8b4bac7e36b
tcrane <thomasgcrane@gmail.com>
2017-02-26 15:42:52 +1100

    Added properties to Configuration model

--- FameBot/Data/Models/Configuration.cs
+++ FameBot/Data/Models/Configuration.cs
@@ -8,6 +8,14 @@ namespace FameBot.Data.Models
 {
     public class Configuration
     {
+        public float AutonexusThreshold { get; set; }
+        public int TickCountThreshold { get; set; }
+        public bool EscapeIfNoTargets { get; set; }
+        public float TeleportDistanceThreshold { get; set; }
+        public float FollowDistanceThreshold { get; set; }
 
+        public bool FindClustersNearCenter { get; set; }
+        public float Epsilon { get; set; }
+        public int MinPoints { get; set; }
     }
 }
