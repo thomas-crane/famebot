a52f6840e3524a8c9dee601a136dad686b408d02
tcrane <thomasgcrane@gmail.com>
2017-02-26 16:29:31 +1100

    Added methods to ConfigManager:  - Added functionality to GetConfiguration, this will return the configuration contained in the saved file. If no saved file exists one will be created with some default settings  - Added WriteXML to write new settings  - Added ReadXML to read the settings

--- FameBot/Services/ConfigManager.cs
+++ FameBot/Services/ConfigManager.cs
@@ -1,13 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FameBot.Data.Models;
+using System.Xml.Serialization;
+using System.Xml;
 
 namespace FameBot.Services
 {
     public static class ConfigManager
     {
+        public static void Test()
+        {
+            var cwd = Directory.GetCurrentDirectory();
+            Console.WriteLine(cwd);
+        }
 
+        public static Configuration GetConfiguration()
+        {
+            string path = Directory.GetCurrentDirectory() + "\\Plugins\\famebot_config.xml";
+
+            if(!File.Exists(path))
+            {
+                Configuration cfg = new Configuration()
+                {
+                    AutonexusThreshold = 0.45f,
+                    TickCountThreshold = 10,
+                    TeleportDistanceThreshold = 15f,
+                    FollowDistanceThreshold = 1.5f,
+                    FindClustersNearCenter = true,
+                    Epsilon = 8f,
+                    MinPoints = 5,
+                    EscapeIfNoTargets = true
+                };
+                WriteXML(cfg);
+                return cfg;
+            } else
+            {
+                return ReadXML();
+            }
+        }
+
+        public static void WriteXML(Configuration cfg)
+        {
+            XmlSerializer xmlS = new XmlSerializer(typeof(Configuration));
+            string path = Directory.GetCurrentDirectory() + "\\Plugins\\famebot_config.xml";
+
+            using (FileStream file = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                xmlS.Serialize(file, cfg);
+            }
+        }
+
+        public static Configuration ReadXML()
+        {
+            XmlSerializer xmlS = new XmlSerializer(typeof(Configuration));
+            string path = Directory.GetCurrentDirectory() + "\\Plugins\\famebot_config.xml";
+
+            Configuration config = new Configuration();
+
+            using (FileStream file = File.Open(path, FileMode.Open, FileAccess.Read))
+            {
+                config = (Configuration)xmlS.Deserialize(file);
+            }
+            return config;
+        }
     }
 }
