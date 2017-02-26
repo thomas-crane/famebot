4916ba8c5eebacd209026c7d72de8a9eac47e3e8
tcrane <thomasgcrane@gmail.com>
2017-02-26 11:18:01 +1100

    Added HealthChangedEventArgs

---
+++ FameBot/Data/Events/HealthChangedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FameBot.Data.Events
+{
+    public class HealthChangedEventArgs : EventArgs
+    {
+        private float health;
+        public float Health
+        {
+            get { return health; }
+        }
+
+        public HealthChangedEventArgs(float health)
+        {
+            this.health = health;
+        }
+    }
+}
