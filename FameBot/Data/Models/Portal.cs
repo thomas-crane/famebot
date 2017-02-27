91f75885b2fb1786564b702013d07914898675f6
tcrane <thomasgcrane@gmail.com>
2017-02-27 20:59:03 +1100

    Added Location property to Portals

--- FameBot/Data/Models/Portal.cs
+++ FameBot/Data/Models/Portal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lib_K_Relay.Networking.Packets.DataObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,13 +12,15 @@ namespace FameBot.Data.Models
         public int ObjectId { get; set; }
         public int PlayerCount { get; set; }
         public string Name { get; set; }
+        public Location Location { get; set; }
         public bool Connected { get; set; }
 
-        public Portal(int objectId, int playerCount, string name)
+        public Portal(int objectId, int playerCount, string name, Location location)
         {
             ObjectId = objectId;
             PlayerCount = playerCount;
             Name = name;
+            Location = location;
         }
     }
 }
