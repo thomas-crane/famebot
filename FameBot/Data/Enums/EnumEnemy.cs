057e94abeb03a50f15d162d2d0b4d4425d4c6db7
tcrane <thomasgcrane@gmail.com>
2017-03-13 20:05:46 +1100

    WIP - enemy avoidance  - Added EnumEnemy to hold enemy ObjectTypes  - Added LocationExtensions for intersection methods

---
+++ FameBot/Data/Enums/EnumEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FameBot.Data.Enums
+{
+    public enum Enemy
+    {
+        WhiteDemon = 1617,
+        SpriteGod = 1618,
+        Medusa = 1920,
+        EntGod = 1621,
+        Beholder = 1622,
+        FlyingBrain = 1623,
+        SlimeGod = 1624,
+        GhostGod = 1625
+    }
+}
---
+++ FameBot/Helpers/LocationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib_K_Relay.Networking.Packets.DataObjects;
+
+namespace FameBot.Helpers
+{
+    public static class LocationExtensions
+    {
+        public static bool IntersectsRadius(this Location location, Location location2, float radius)
+        {
+            return location.DistanceTo(location2) <= radius;
+        }
+        public static bool IntersectsRect(this Location location, Location location2, float width)
+        {
+            return location.X - width >= location2.X
+                && location.X + width <= location2.X
+                && location.Y + width <= location2.Y
+                && location.Y - width >= location2.Y;
+        }
+    }
+}
