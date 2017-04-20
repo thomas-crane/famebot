d073a8620075745f2486b35172b016a85d6ccd2f
tcrane <thomasgcrane@gmail.com>
2017-04-20 21:38:32 +1000

    Added ConnectTo to ClientExtensions  - Added a ConnectTo to correctly send a reconnect packet.

--- FameBot/Helpers/ClientExtensions.cs
+++ FameBot/Helpers/ClientExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Lib_K_Relay.Networking;
+using Lib_K_Relay.Networking.Packets.Server;
 using Lib_K_Relay.Utilities;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,28 @@ namespace FameBot.Helpers
         public static void Notify(this Client client, string text)
         {
             client.SendToClient(PluginUtils.CreateNotification(client.ObjectId, text));
+        }
+
+        public static bool ConnectTo(this Client client, ReconnectPacket packet)
+        {
+            if (packet == null)
+                return false;
+            string host = packet.Host;
+            int port = packet.Port;
+            byte[] key = packet.Key;
+            client.State.ConTargetAddress = host;
+            client.State.ConTargetPort = port;
+            client.State.ConRealKey = key;
+            packet.Key = Encoding.UTF8.GetBytes(client.State.GUID);
+            packet.Host = "localhost";
+            packet.Port = 2050;
+
+            client.SendToClient(packet);
+
+            packet.Key = key;
+            packet.Host = host;
+            packet.Port = port;
+            return true;
         }
     }
 }
