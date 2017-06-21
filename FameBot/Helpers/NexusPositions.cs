91b81711767548d0a74053bb94fb53abec208475
thomas-crane <thomasgcrane@gmail.com>
2017-06-21 19:22:25 +1000

    Updated nexus positions

--- FameBot/Helpers/NexusPositions.cs
+++ FameBot/Helpers/NexusPositions.cs
@@ -9,7 +9,7 @@ namespace FameBot.Helpers
 {
     public static class NexusPositions
     {
-        public static readonly Location Realms = new Location(159, 101);
-        public static readonly Location Fountains = new Location(159, 127);
+        public static readonly Location Realms = new Location(107, 137);
+        public static readonly Location Fountains = new Location(107, 158);
     }
 }
