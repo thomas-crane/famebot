979037b3e8457c4fb05a716de70808458c71fe3b
thomas-crane <thomasgcrane@gmail.com>
2017-06-21 17:59:40 +1000

    Added a class to hold the nexus positions.

---
+++ FameBot/Helpers/NexusPositions.cs
@@ -0,0 +1,15 @@
+﻿using Lib_K_Relay.Networking.Packets.DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FameBot.Helpers
+{
+    public static class NexusPositions
+    {
+        public static readonly Location Realms = new Location(159, 101);
+        public static readonly Location Fountains = new Location(159, 127);
+    }
+}
