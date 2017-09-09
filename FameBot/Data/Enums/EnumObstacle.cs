2c6b6d75fd7da53031557a2c373503c39e152fa0
thomas-crane <thomasgcrane@gmail.com>
2017-09-09 12:20:41 +1000

    Added EnumObstacle to hold obstacle ObjectIds

---
+++ FameBot/Data/Enums/EnumObstacle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FameBot.Data.Enums
+{
+    public enum ObstacleId
+    {
+        Rock = 263
+    }
+}
